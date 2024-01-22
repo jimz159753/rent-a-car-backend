50b02f7d3a87f5ea7d998b6e4b46fdadcc3173ee
Luis Jimenez <luisjc140992@gmail.com>
2024-01-21 21:29:30 -0600

    Format birthday in clients

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -11,6 +11,7 @@ export class ClientsService {
   constructor(@InjectModel(Client.name) private clientModel: Model<Client>) { }
 
   async create(createClientDto: CreateClientDto): Promise<Client> {
+    createClientDto.birthday = dayjs(createClientDto.birthday).format('DD-MM-YYYY')
     createClientDto.timestamp = dayjs().format('DD-MM-YYYY hh:mm a');
     createClientDto.lastUpdate = dayjs().format('DD-MM-YYYY hh:mm a');
     const response = new this.clientModel(createClientDto);
